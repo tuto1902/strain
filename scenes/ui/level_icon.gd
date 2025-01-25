851a124be4d3f0174a84a00061d9eca598dc9096
Arturo Rojas <jorge_arturo_rojas@hotmail.com>
2025-01-25 14:43:10 -0600

    Level Selection Logic, Music and Sound Fx
    - Added level locked, unlocked and cleared status to each level node
    - Added logic to set the current level as cleared and unlock the connected levels
    - Added music and sound fx

--- scenes/ui/level_icon.gd
+++ scenes/ui/level_icon.gd
@@ -3,10 +3,9 @@ extends VBoxContainer
 
 @export var level_index: int = 1
 @export var level_name: String = ""
-@export var level_icon: AtlasTexture
 
 @onready var label: Label = $Label
-@onready var texture_rect: TextureRect = $TextureRect
+@onready var node_icon = $NodeIcon
 @onready var player_anchor: Marker2D = $PlayerAnchor
 
 @export var level_left: Level
@@ -14,13 +13,42 @@ extends VBoxContainer
 @export var level_up: Level
 @export var level_down: Level
 
+@export_enum("Unlocked", "Locked", "Cleared") var level_status = "Unlocked"
+@export var locked_icon_texture: AtlasTexture
+@export var unlocked_icon_texture: AtlasTexture
+@export var cleared_icon_texture: AtlasTexture
+
 func _ready():
 	label.text = level_name
-	if level_icon != null:
-		texture_rect.texture = level_icon
+	match level_status:
+		"Unlocked":
+			if unlocked_icon_texture:
+				node_icon.texture = unlocked_icon_texture
+		"Locked":
+			if locked_icon_texture:
+				node_icon.texture = locked_icon_texture
+		"Cleared":
+			if cleared_icon_texture:
+				node_icon.texture = cleared_icon_texture
 
 func _process(_delta):
 	if Engine.is_editor_hint():
 		label.text = level_name
-		if level_icon != null:
-			texture_rect.texture = level_icon
+		match level_status:
+			"Unlocked":
+				if unlocked_icon_texture:
+					node_icon.texture = unlocked_icon_texture
+			"Locked":
+				if locked_icon_texture:
+					node_icon.texture = locked_icon_texture
+			"Cleared":
+				if cleared_icon_texture:
+					node_icon.texture = cleared_icon_texture
+
+func unlock():
+	level_status = "Unlocked"
+	node_icon.texture = unlocked_icon_texture
+
+func level_cleared():
+	level_status = "Cleared"
+	node_icon.texture = cleared_icon_texture
--- scenes/ui/level_select.gd
+++ scenes/ui/level_select.gd
@@ -2,6 +2,8 @@ extends Control
 
 @export var start_level: Level
 @onready var player_icon: Sprite2D = $PlayerIcon
+@onready var level_select_sfx = $LevelSelectSfx
+@onready var level_confirm_sfx = $LevelConfirmSfx
 
 var current_level: Level
 var icon_tween: Tween
@@ -13,16 +15,38 @@ func _ready():
 func _input(event):
 	if icon_tween and icon_tween.is_running():
 		return
+	if event.is_action_pressed("ui_accept"):
+		on_level_cleared()
 	
-	if event.is_action_pressed("ui_left") and current_level.level_left != null:
+	if event.is_action_pressed("ui_left") and current_level.level_left != null and current_level.level_left.level_status == "Unlocked":
 		current_level = current_level.level_left
-	elif event.is_action_pressed("ui_right") and current_level.level_right != null:
+	elif event.is_action_pressed("ui_right") and current_level.level_right != null and current_level.level_right.level_status == "Unlocked":
 		current_level = current_level.level_right
-	elif event.is_action_pressed("ui_up") and current_level.level_up != null:
+	elif event.is_action_pressed("ui_up") and current_level.level_up != null and current_level.level_up.level_status == "Unlocked":
 		current_level = current_level.level_up
-	elif event.is_action_pressed("ui_down") and current_level.level_down != null:
+	elif event.is_action_pressed("ui_down") and current_level.level_down != null and current_level.level_down.level_status == "Unlocked":
 		current_level = current_level.level_down
+	else:
+		return
 	
 	icon_tween = get_tree().create_tween()
 	icon_tween.tween_property(player_icon, "global_position", current_level.player_anchor.global_position, 0.3).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CUBIC)
-	#player_icon.global_position = current_level.player_anchor.global_position
+	icon_tween.finished.connect(on_level_select)
+
+func unlock_connected_levels():
+	if current_level.level_left != null and current_level.level_left.level_status == "Locked":
+		current_level.level_left.unlock()
+	if current_level.level_right != null and current_level.level_right.level_status == "Locked":
+		current_level.level_right.unlock()
+	if current_level.level_up != null and current_level.level_up.level_status == "Locked":
+		current_level.level_up.unlock()
+	if current_level.level_down != null and current_level.level_down.level_status == "Locked":
+		current_level.level_down.unlock()
+
+func on_level_cleared():
+	current_level.level_cleared()
+	level_confirm_sfx.play()
+	unlock_connected_levels()
+
+func on_level_select():
+	level_select_sfx.play()
